4ebb0e6b74d9a81db13cbb3ef31b2aed1463da64
anithapatteda <apatteda@stevens.edu>
2015-02-11 11:07:38 -0500

    Update project03.java

--- project03.java
+++ project03.java
@@ -19,26 +19,65 @@ public class project03 {
 	    
 	   // Charset charset = Charset.forName("UTF-8");
 	    String line;
-	    HashMap<String, String> TagValue = new HashMap<String, String>(); 
+	   // HashMap<String, String> TagValue = new HashMap<String, String>(); 
 	       for (int i = 0; i < listOfFiles.length; i++) {
 	    	  // System.out.println(listOfFiles[i]);
 
 	      	   files = listOfFiles[i].getName();
 	      	   
 	    	   if (files.endsWith(".txt") || files.endsWith(".TXT")){
-	    		   System.out.println(files);
+	    		   String filename = files.toUpperCase();
+	    		   System.out.println("START OF FILE :"+filename);
 	    		   try{
 	    		   BufferedReader br = new BufferedReader(new FileReader(listOfFiles[i]));
+	    		   HashMap NameId = new HashMap();
+	    		   String name = null;
+    			   String id = null;
 	    		   while ((line = br.readLine()) != null ) {
+	    			  
+	    			   //System.out.println("Line = "+line);
 	    			   String[] lineVariables = line.split(" "); 
 	    			   if(lineVariables.length == 3){
-	    				   TagValue.put(lineVariables[1], lineVariables[2]);
-	    				   System.out.println(TagValue);
+	    				  if(lineVariables[1].startsWith("@") && lineVariables[2].equals("INDI") ){
+	    					  id=lineVariables[1];
+	    				  }else if(lineVariables[1].equals("SUBM")){
+	    					  id=lineVariables[2];
+	    				  }
+	    				  
+	    				  if(lineVariables[1].equals("GIVN")){
+	    					  name = lineVariables[2];
+	    				  }
+	    				 // System.out.println(name+id);
+	    				  if(id!=null && name!=null){
+	    				    NameId.put(id,name);
+	    				   // System.out.println(NameId);
+	    				  }
+	    				  if(lineVariables[2].equals("FAM")){
+	    					  System.out.println("-------------------");
+	    					  System.out.println(" Family ID :"+lineVariables[1]);
+	    				  }
+	    				  if(lineVariables[1].equals("HUSB")){
+	    					 String husband = (String) NameId.get(lineVariables[2]);
+	    					 System.out.println("Unique Id :"+lineVariables[2]+" "+" Husband Name :"+husband);
+	    				  }
+	    				  if(lineVariables[1].equals("WIFE")){
+		    					 String wife = (String) NameId.get(lineVariables[2]);
+		    					 System.out.println("Unique Id :"+lineVariables[2]+" "+" Wife Name :"+wife);
+		    					 
+		    			  }
+	    				  if(lineVariables[1].equals("CHIL")){
+	    					  String child = (String) NameId.get(lineVariables[2]);
+		    					System.out.println("Unique Id :"+lineVariables[2]+" "+" Child Name :"+child);
+		    					
+	    				  }
+	    				 
 	    			   }
 	    		   }
 	    		   }catch(Exception e){
 	    			   
 	    		   }
+	    		   System.out.println("END OF FAMILY : "+filename);
+	    		   System.out.println("             ");
 	    	   }
 	               
 	           
