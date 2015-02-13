917f1deee85061a4d8627ffb31582c9572b9e450
qingliaowu <qingliaowu@gmail.com>
2015-02-12 19:55:04 -0500

    Update gedcom_main.java
    I fixed the main function   Qingliao

--- project/gedcom_main.java
+++ project/gedcom_main.java
@@ -1,48 +1,18 @@
-package Agile;
+import java.io.*;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.HashMap;
+import ssw555.project.input_store;
 
-public class project03 {
-	public static void main(String args[]){
-		ReadFiles();
-	}
-	
-	public static void ReadFiles(){
-		String files;
-	    File folder = new File("C:/eclipse/Programmes/Agile");
-	    File[] listOfFiles = folder.listFiles(); 
-	    
-	   // Charset charset = Charset.forName("UTF-8");
-	    String line;
-	    HashMap<String, String> TagValue = new HashMap<String, String>(); 
-	       for (int i = 0; i < listOfFiles.length; i++) {
-	    	  // System.out.println(listOfFiles[i]);
-
-	      	   files = listOfFiles[i].getName();
-	      	   
-	    	   if (files.endsWith(".txt") || files.endsWith(".TXT")){
-	    		   System.out.println(files);
-	    		   try{
-	    		   BufferedReader br = new BufferedReader(new FileReader(listOfFiles[i]));
-	    		   while ((line = br.readLine()) != null ) {
-	    			   String[] lineVariables = line.split(" "); 
-	    			   if(lineVariables.length == 3){
-	    				   TagValue.put(lineVariables[1], lineVariables[2]);
-	    				   System.out.println(TagValue);
-	    			   }
-	    		   }
-	    		   }catch(Exception e){
-	    			   
-	    		   }
-	    	   }
-	               
-	           
-	      }
-	}
-
+public class gedcom_main {
+ public static void main(String args[]) {
+  try {
+  input_store gcfr = new input_store();
+   gcfr.readFile("C:/QingliaoWu.ged");
+   System.out.println("Individuals:");
+   gcfr.print_individual();
+   System.out.println("Families:");
+   gcfr.print_family();
+  } catch (IOException e) {
+   e.printStackTrace();
+  }
+ }
 }
