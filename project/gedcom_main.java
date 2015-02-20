5f3a0e5ef037d85c7816ca83e669b99ea61ae2f6
LakshmiRoja <roja.tirumala@gmail.com>
2015-02-19 20:02:52 -0500

    Update gedcom_main.java

--- project/gedcom_main.java
+++ project/gedcom_main.java
@@ -8,11 +8,11 @@ import ssw555.project.input_store;
 public class gedcom_main {
  public static void main(String args[]) {
   try {
-  input_store gedcomr = new input_store();
-   gedcomr.readFile("C:/QingliaoWu.ged");
-   System.out.println("Individuals:");
+  input_store gedcomr = new input_store(); // creates object for gedcom file 
+   gedcomr.readFile("C:/QingliaoWu.ged"); // Read file
+   System.out.println("Individuals:"); // Prints individual names
    gedcomr.print_individual();
-   System.out.println("Families:");
+   System.out.println("Families:"); // Prints family
    gedcomr.print_family();
   } catch (IOException e) {
    e.printStackTrace();
